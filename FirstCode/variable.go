d9e0df4f67998c84a142042a9bec60be7f7fbecf
atowarrahman <emonatowarrahman@gmail\.com>
2021-02-01 22:53:40 +0600

    second commit

--- FirstCode/variable.go
+++ FirstCode/variable.go
@@ -2,6 +2,10 @@ package main
 import "fmt"
 func main(){
   var name string="Emon";
-  fmt.Println(name);
+  fmt.Println(name); 
+  var c rune='A';
+  var age int=20;
+  var cgpa float32=3.67;
+  fmt.Println(c,age,cgpa);
 
 }
